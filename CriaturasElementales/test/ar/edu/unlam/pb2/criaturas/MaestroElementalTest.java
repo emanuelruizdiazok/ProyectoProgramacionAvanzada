78316cddfe74879de371caa25cf5eefe5107bc41
Emanuel Ruiz Díaz <emanuelruizdiazok@gmail.com>
2025-11-19 23:20:44 -0300

    Se agregaron los test de MaestroElemental y Transformaciones

---
+++ CriaturasElementales/test/ar/edu/unlam/pb2/criaturas/MaestroElementalTest.java
@@ -0,0 +1,50 @@
+package ar.edu.unlam.pb2.criaturas;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MaestroElementalTest {
+
+    @Test
+    public void queSePuedaAgregarCriaturaAlMaestro() {
+        MaestroElemental maestro = new MaestroElemental("Zuko", 12, Afinidad.FUEGO);
+        Criatura criatura = new CriaturaDomesticada("Draco", 80, Afinidad.FUEGO);
+        maestro.agregarCriatura(criatura);
+        assertNotNull(maestro.getCriatura("Draco"));
+    }
+
+    @Test
+    public void queMaestroPuedaEntrenarCriaturaSiTieneSuficienteMaestria() throws FaltaDeMaestriaException {
+        MaestroElemental maestro = new MaestroElemental("Iroh", 15, Afinidad.FUEGO);
+        Criatura criatura = new CriaturaDomesticada("Fenix", 100, Afinidad.FUEGO);
+        maestro.agregarCriatura(criatura);
+        maestro.entrenar("Fenix");
+        assertEquals(120, maestro.getCriatura("Fenix").getEnergia());
+    }
+
+    @Test(expected = FaltaDeMaestriaException.class)
+    public void queLanceFaltaDeMaestriaExceptionSiNoTieneNivelSuficiente() throws FaltaDeMaestriaException {
+        MaestroElemental maestro = new MaestroElemental("Novato", 5, Afinidad.AGUA);
+        Criatura criatura = new CriaturaDomesticada("Nami", 90, Afinidad.AGUA);
+        maestro.agregarCriatura(criatura);
+        maestro.entrenar("Nami");
+    }
+
+    @Test
+    public void queMaestroPuedaPacificarCriatura() {
+        MaestroElemental maestro = new MaestroElemental("Katara", 20, Afinidad.AGUA);
+        Criatura criatura = new CriaturaAncestral("Poseidon", 150, Afinidad.AGUA);
+        criatura.setInestable(true);
+        maestro.agregarCriatura(criatura);
+        maestro.pacificar("Poseidon");
+        assertFalse(maestro.getCriatura("Poseidon").esInestable());
+    }
+
+    @Test
+    public void queGettersFuncionenCorrectamente() {
+        MaestroElemental maestro = new MaestroElemental("Toph", 18, Afinidad.TIERRA);
+        assertEquals("Toph", maestro.getNombre());
+        assertEquals(Integer.valueOf(18), maestro.getNivelDeMaestria());
+        assertEquals(Afinidad.TIERRA, maestro.getAfinidad());
+    }
+}
---
+++ CriaturasElementales/test/ar/edu/unlam/pb2/criaturas/TransformacionesTest.java
@@ -0,0 +1,48 @@
+package ar.edu.unlam.pb2.criaturas;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TransformacionesTest {
+
+    @Test
+    public void queLlamaInternaSume10DeEnergiaAlEntrenar() {
+        Criatura base = new CriaturaDomesticada("Fuego", 50, Afinidad.FUEGO);
+        Criatura decorada = new LlamaInterna(base);
+        decorada.entrenar();
+        assertEquals(60, base.getEnergia());
+    }
+
+    @Test
+    public void queBendicionDelRioSume5DeEnergiaAlEntrenar() {
+        Criatura base = new CriaturaDomesticada("Agua", 30, Afinidad.AGUA);
+        Criatura decorada = new BendicionDelRio(base);
+        decorada.entrenar();
+        assertEquals(35, base.getEnergia());
+    }
+
+    @Test
+    public void queAscensoDelVientoSume15DeEnergiaAlEntrenar() {
+        Criatura base = new CriaturaDomesticada("Viento", 20, Afinidad.AIRE);
+        Criatura decorada = new AscensoDelViento(base);
+        decorada.entrenar();
+        assertEquals(35, base.getEnergia());
+    }
+
+    @Test
+    public void queVinculoTerrestreSume7DeEnergiaAlEntrenar() {
+        Criatura base = new CriaturaDomesticada("Tierra", 40, Afinidad.TIERRA);
+        Criatura decorada = new VinculoTerrestre(base);
+        decorada.entrenar();
+        assertEquals(47, base.getEnergia());
+    }
+
+    @Test
+    public void queTransformacionMantengaEstabilidadDeLaCriaturaOriginal() {
+        Criatura base = new CriaturaSalvaje("Salvaje", 100, Afinidad.FUEGO);
+        base.setInestable(true);
+        Criatura decorada = new LlamaInterna(base);
+        decorada.pacificar();
+        assertFalse(base.esInestable());
+    }
+}
