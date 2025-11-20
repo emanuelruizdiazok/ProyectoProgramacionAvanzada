fd6bcb75c6c26e0d8a9ab0b0ffa8c3064481969d
juanmapena <juanmanuelpena180@gmail.com>
2025-11-19 22:05:43 -0300

    Se agregan dos clases mas de criatura salvaje y ancestral con sus debidos metodos

---
+++ CriaturasElementales/src/ar/edu/unlam/pb2/criaturas/CriaturaAncestral.java
@@ -0,0 +1,20 @@
+package ar.edu.unlam.pb2.criaturas;
+
+public class CriaturaAncestral extends Criatura {
+
+    public CriaturaAncestral(String nombre, int energia, Afinidad afinidad) {
+        super(nombre, energia, afinidad);
+    }
+
+    @Override
+    public void entrenar() {
+        modificarEnergia(50);
+    }
+
+    @Override
+    public void pacificar() {
+        this.inestable = false;
+        modificarEnergia(10);
+    }
+}
+
---
+++ CriaturasElementales/src/ar/edu/unlam/pb2/criaturas/CriaturaDomesticada.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.pb2.criaturas;
+
+public class CriaturaDomesticada extends Criatura {
+
+    public CriaturaDomesticada(String nombre, int energia, Afinidad afinidad) {
+        super(nombre, energia, afinidad);
+    }
+
+    @Override
+    public void entrenar() {
+        modificarEnergia(20);
+    }
+
+    @Override
+    public void pacificar() {
+        this.inestable = false;
+    }
+}
